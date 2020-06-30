bf7492f31ea502e71d99b246ff27a31bf3eb2087
pry0cc <pry0cc@0x00sec.org>
2020-06-30 14:26:13 -0400

    added example scope file

--- pscan.sh
+++ pscan.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 name="example-scan"
-
+scope="ranges.txt"
 # Start fleet using the supplied name, spend $0.1 and self-destruct after 1 hour
 axiom-fleet $name -i=5 --spend=0.1 --time=1
 
+# Upload the scope to every single host... If this is really big, do some magic with split.
+for i in $(axiom-ls -d | grep -E "$name*"); do axiom-scp $scope $i:~/ranges.txt .; done
+
 # Execute this one liner on every machine, basically scan its portion
-axiom-execb 'curl -s https://easyasn.xyz/companies/vultr/ranges.txt | grep -v ":" > ranges.txt && sudo masscan -iL ranges.txt --rate=10000 -p443 --shard $i/$total -oG $name.txt' "$name*" 
+axiom-execb 'sudo masscan -iL ranges.txt --rate=10000 -p443 --shard $i/$total -oG $name.txt' "$name*" 
 
 
 # Wait until the scan has finished, then press enter to tear down!
